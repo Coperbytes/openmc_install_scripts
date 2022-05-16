b1ca69a3180fc16103bac0467cca4622f8c22acf
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-05-16 09:23:51 +0200

    add debug flag to compile

--- Arch/dagmc-install.sh
+++ Arch/dagmc-install.sh
@@ -54,6 +54,7 @@ if [ ! -e ${name}.done ]; then
                -DBUILD_STATIC_LIBS=OFF\
                -DCMAKE_INSTALL_PREFIX=${install_prefix}\
                -DDOUBLE_DOWN_DIR=${install_prefix}
+               -DCMAKE_BUILD_TYPE=Debug\
   make -j $ccores
   make install
 

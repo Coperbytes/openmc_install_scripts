1202765e870601367dbf9b1b9187cfd416b5b79d
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-01-11 11:59:15 +0100

    merge from main

--- Debian11/dagmc-install.sh
+++ Debian11/dagmc-install.sh
@@ -12,29 +12,45 @@ WD=`pwd`
 name=`basename $0`
 package_name='dagmc'
 
+install_prefix="/opt"
+if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
+  install_prefix=$LOCAL_INSTALL_PREFIX
+fi
+build_prefix="$HOME/openmc"
+
 #if there is a .done-file then skip this step
-if [ ! -e $0.done ]; then
+if [ ! -e ${name}.done ]; then
 
   sudo apt-get install --yes python3
 
-  cd $HOME/openmc
-  mkdir DAGMC
-  cd DAGMC
-  git clone --single-branch --branch develop --depth 1 https://github.com/svalinn/DAGMC.git
-  mkdir build
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
+
+  mkdir -p $HOME/openmc/DAGMC
+  cd $HOME/openmc/DAGMC
+  if [ ! -e DAGMC ]; then
+    git clone --single-branch --branch develop --depth 1 https://github.com/svalinn/DAGMC.git
+  else
+    cd DAGMC; git pull; cd ..
+  fi
+
+  mkdir -p build
   cd build
   cmake ../DAGMC -DBUILD_TALLY=ON \
-               -DMOAB_DIR=$HOME/openmc/MOAB \
-               -DDOUBLE_DOWN=ON \
-               -DBUILD_STATIC_EXE=OFF \
-               -DBUILD_STATIC_LIBS=OFF \
-               -DCMAKE_INSTALL_PREFIX=$HOME/openmc/DAGMC/ \
-               -DDOUBLE_DOWN_DIR=$HOME/openmc/double-down
+               -DMOAB_DIR=${install_prefix}\
+               -DDOUBLE_DOWN=ON\
+               -DBUILD_STATIC_EXE=OFF\
+               -DBUILD_STATIC_LIBS=OFF\
+               -DCMAKE_INSTALL_PREFIX=${install_prefix}\
+               -DDOUBLE_DOWN_DIR=${install_prefix}
+  make -j $ccores
   make install
-  cd $WD
 
-  touch ${0}.done
+  cd ${WD}
+  touch ${name}.done
 else
   echo DAGMC appears already to be installed \(lock file ${name}.done exists\) - skipping.
 fi
-  

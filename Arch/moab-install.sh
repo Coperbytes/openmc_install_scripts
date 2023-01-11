4bd691399a7dd3d04c856a4428ac20b3d89c5a6c
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-01-11 11:52:18 +0100

    edits from main

--- Arch/moab-install.sh
+++ Arch/moab-install.sh
@@ -9,17 +9,22 @@ name=`basename $0`
 package_name='MOAB'
 
 install_prefix="/opt"
+if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
+  install_prefix=$LOCAL_INSTALL_PREFIX
+fi
+
+echo will install to $LOCAL_INSTALL_PREFIX
 
 #check if there is a .done file indicating that we have already built this target
 if [ ! -e ${name}.done ]; then
-
-  sudo pacman -Sy --noconfirm \
+  if ! pacman -Qi eigen netcdf hdf5-openmpi python-setuptools cython; then
+    sudo pacman -Sy --noconfirm \
 	eigen \
 	netcdf \
 	hdf5 \
         python-setuptools \
 	cython
-
+  fi
   #Should we run make in parallel? Default is to use all available cores
   ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
   if [ "x$1" != "x" ]; then
@@ -28,16 +33,22 @@ if [ ! -e ${name}.done ]; then
 
   mkdir -p $HOME/openmc/MOAB
   cd $HOME/openmc/MOAB
-  git clone --single-branch --branch 5.3.1 --depth 1 https://bitbucket.org/fathomteam/moab.git
+  if [ ! -e moab ]; then
+    git clone --single-branch --branch 5.3.1 --depth 1 https://bitbucket.org/fathomteam/moab.git
+  else
+    cd moab
+    git pull
+    cd ..
+  fi
   mkdir -p build
   cd build
   cmake ../moab -DENABLE_HDF5=ON \
-              -DCMAKE_BUILS_TYPE=Debug\
 	      -DENABLE_PYMOAB=ON \
               -DENABLE_FORTRAN=OFF \
               -DBUILD_SHARED_LIBS=ON \
               -DENABLE_BLASLAPACK=OFF \
-              -DCMAKE_INSTALL_PREFIX=${install_prefix}/MOAB
+              -DCMAKE_BUILD_TYPE=Debug \
+              -DCMAKE_INSTALL_PREFIX=${install_prefix}
   make -j ${ccores}
   make install
 

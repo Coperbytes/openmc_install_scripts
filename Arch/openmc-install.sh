334aa4557696f45b2b6a07fa5276da2d5f38e8ef
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-01-11 10:43:14 +0100

    add missing dagmc root

--- Arch/openmc-install.sh
+++ Arch/openmc-install.sh
@@ -82,6 +82,7 @@ if [ ! -e ${name}.done ]; then
   mkdir -p build
   cd build
   cmake -DOPENMC_USE_DAGMC=ON\
+        -DDAGMC_ROOT=${install_prefix}\
         -DOPENMC_USE_OPENMP=OFF\
         -DOPENMC_USE_MPI=off\
         -DHDF5_PREFER_PARALLEL=off\

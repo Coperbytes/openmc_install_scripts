ec93a45c27a1997e416745a6d7429828728029db
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-01-11 14:10:25 +0100

    update to use 5.4.1 instead

--- Debian11/moab-install.sh
+++ Debian11/moab-install.sh
@@ -3,16 +3,19 @@
 ################################################################################
 #!/bin/bash
 set -ex
-if [ "x" == "$1x" ]; then
-	ccores=1
-else
-	ccores=$1
-fi
 
 WD=`pwd`
 name=`basename $0`
+package_name='MOAB'
 
-#if there is a .done-file then skip this step
+install_prefix="/opt"
+if [ "x" != "x$LOCAL_INSTALL_PREFIX" ]; then
+  install_prefix=$LOCAL_INSTALL_PREFIX
+fi
+
+echo will install to $LOCAL_INSTALL_PREFIX
+
+#check if there is a .done file indicating that we have already built this target
 if [ ! -e ${name}.done ]; then
 
   sudo apt-get install --yes gcc\
@@ -30,32 +33,40 @@ if [ ! -e ${name}.done ]; then
         libhdf5-dev \
         python3-setuptools\
 	cython3
+  #Should we run make in parallel? Default is to use all available cores
+  ccores=`cat /proc/cpuinfo |grep CPU|wc -l`
+  if [ "x$1" != "x" ]; then
+	ccores=$1
+  fi
 
-  cd $HOME
-  mkdir -p openmc
-  cd openmc
-  mkdir -p MOAB
-  cd MOAB
-  git clone  --single-branch --branch 5.3.1 --depth 1 https://bitbucket.org/fathomteam/moab.git
+  mkdir -p $HOME/openmc/MOAB
+  cd $HOME/openmc/MOAB
+  if [ ! -e moab ]; then
+    git clone --single-branch --branch 5.4.1 --depth 1 https://bitbucket.org/fathomteam/moab.git
+  else
+    cd moab
+    git pull
+    cd ..
+  fi
   mkdir -p build
   cd build
   cmake ../moab -DENABLE_HDF5=ON \
               -DENABLE_NETCDF=ON \
+	      -DENABLE_PYMOAB=ON \
               -DENABLE_FORTRAN=OFF \
-              -DENABLE_BLASLAPACK=OFF \
               -DBUILD_SHARED_LIBS=ON \
-              -DENABLE_PYMOAB=OFF \
-              -DCMAKE_INSTALL_PREFIX=$HOME/openmc/MOAB
-  make -j $ccores
+              -DENABLE_BLASLAPACK=OFF \
+              -DCMAKE_BUILD_TYPE=Debug \
+              -DCMAKE_INSTALL_PREFIX=${install_prefix}
+  make -j ${ccores}
   make install
 
-  #cd pymoab
-  #bash install.sh
-  #sudo python3 setup.py install
+  #to install the python API
+  cd pymoab
+  bash install.sh
 
-  cd $WD
-
+  cd ${WD}
   touch ${name}.done
 else
-  echo MOAB appears to be already installed \(lock file ${name}.done exists\) - skipping.
+  echo ${package_name} appears to be already installed \(lock file ${name}.done exists\) - skipping.
 fi
